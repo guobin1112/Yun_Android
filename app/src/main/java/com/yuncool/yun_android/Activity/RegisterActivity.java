6e410d36278c22155c043a1c4a2bbc658db94693
Black <guobin1112@yeah.net>
2016-01-20 14:45:04 +0800

    add activity and fragment

---
+++ app/src/main/java/com/yuncool/yun_android/Activity/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.yuncool.yun_android.Activity;
+
+import android.os.Bundle;
+
+import com.yuncool.yun_android.R;
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
