b27db02f76ca63be1c75aec40893470e86a5cf7a
Black <guobin1112@yeah.net>
2016-02-12 22:28:45 +0800

    1.add MainActivity fragment replace 2.FavorableShopFragment layout completing

---
+++ app/src/main/java/com/yuncool/yun_android/adapter/ShopAdapter.java
@@ -0,0 +1,59 @@
+package com.yuncool.yun_android.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yuncool.yun_android.R;
+import com.yuncool.yun_android.model.ShopModel;
+
+import java.util.List;
+
+/**
+ * Created by Black on 2016/2/12.
+ */
+public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ViewHolder> {
+
+    private List<ShopModel> modelList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView iv_shop;
+        public TextView tv_shop;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            iv_shop = (ImageView) itemView.findViewById(R.id.iv_shop);
+            tv_shop = (TextView) itemView.findViewById(R.id.tv_shop);
+        }
+
+
+    }
+
+    public ShopAdapter(List<ShopModel> modelList) {
+        this.modelList = modelList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shop, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.iv_shop.setImageResource(modelList.get(position).shopImageResId);
+        holder.tv_shop.setText(modelList.get(position).shopName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelList.size();
+    }
+}
---
+++ app/src/main/java/com/yuncool/yun_android/model/ShopModel.java
@@ -0,0 +1,14 @@
+package com.yuncool.yun_android.model;
+
+/**
+ * Created by Black on 2016/2/12.
+ */
+public class ShopModel {
+    public int shopImageResId;
+    public String shopName;
+
+    public ShopModel(int shopImageResId, String shopName) {
+        this.shopImageResId = shopImageResId;
+        this.shopName = shopName;
+    }
+}
