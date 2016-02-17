0e6e66956884566d575d6e9ee36400d65c63a94b
Black <guobin1112@yeah.net>
2016-02-17 23:07:49 +0800

    recommend activity layout finish

--- app/src/main/java/com/yuncool/yun_android/Activity/RecommendActivity.java
+++ app/src/main/java/com/yuncool/yun_android/Activity/RecommendActivity.java
@@ -1,14 +1,69 @@
 package com.yuncool.yun_android.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.yuncool.yun_android.R;
 
-public class RecommendActivity extends BaseActivity {
+public class RecommendActivity extends BaseActivity implements View.OnClickListener {
+
+    private ImageButton ib_wechat_friends, ib_weibo_friends, ib_wechat_circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+
+        initData();
+        initView();
+        initEvent();
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+        ib_wechat_friends = (ImageButton) findViewById(R.id.ib_wechat_friends);
+        ib_wechat_circle = (ImageButton) findViewById(R.id.ib_wechat_circle);
+        ib_weibo_friends = (ImageButton) findViewById(R.id.ib_weibo_friends);
+
+    }
+
+    private void initEvent() {
+
+        ib_wechat_friends.setOnClickListener(this);
+        ib_weibo_friends.setOnClickListener(this);
+        ib_wechat_circle.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.ib_wechat_friends:
+
+                Toast.makeText(RecommendActivity.this, "正在开发中", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.ib_weibo_friends:
+
+                Toast.makeText(RecommendActivity.this, "正在开发中", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.ib_wechat_circle:
+
+                Toast.makeText(RecommendActivity.this, "正在开发中", Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
     }
 }
