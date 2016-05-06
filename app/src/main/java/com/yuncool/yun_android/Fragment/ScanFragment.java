59254ed68c5f28a8c881cfdc0c8af2ca0add8cd5
Black <guobin_xu@yeah.net>
2016-05-07 01:04:08 +0800

    add scan fragment toast

--- app/src/main/java/com/yuncool/yun_android/Fragment/ScanFragment.java
+++ app/src/main/java/com/yuncool/yun_android/Fragment/ScanFragment.java
@@ -7,14 +7,18 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.yuncool.yun_android.MainApplication;
 import com.yuncool.yun_android.R;
+import com.yuncool.yun_android.util.YunSQLiteHelper;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScanFragment extends BaseFragment implements View.OnTouchListener {
 
+    YunSQLiteHelper yunSQLiteHelper;
 
     public ScanFragment() {
         // Required empty public constructor
@@ -29,6 +33,18 @@ public class ScanFragment extends BaseFragment implements View.OnTouchListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
         view.setOnTouchListener(this);
+
+        yunSQLiteHelper = new YunSQLiteHelper(getActivity());
+
+        if (yunSQLiteHelper.getFileList(MainApplication.getLoginUserInfo().userId).size() > 0) {
+            Toast.makeText(getActivity(), "你有" + yunSQLiteHelper.getFileList(MainApplication.getLoginUserInfo().userId).size()
+                    + "份文档代打印,快去附近打印吧！", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getActivity(), "你还没有文档需要打印，快去上传文档打印吧！", Toast.LENGTH_SHORT).show();
+        }
+
+
         return view;
     }
 
